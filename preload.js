34de7e34f68bdc5b0b21970b623a99c3a5d25221
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2025-04-10 17:04:34 -0500

    removed node vibrant usage

--- preload.js
+++ preload.js
@@ -2,5 +2,4 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('myAPI', {
   onUpdateTime: (callback) => ipcRenderer.on('update-time', callback),
-  }
-);
+});
